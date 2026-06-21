a591d2918249bc7ab944c0226e792676acd2cbf4
Onkar Mahamuni <mahamunio59@gmail.com>
2026-06-21 23:29:38 +0530

    Fix TanStack router basepath for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,10 @@ import { createRouter, RouterProvider } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
 import './styles.css'
 
-const router = createRouter({ routeTree })
+const router = createRouter({ 
+  routeTree,
+  basepath: '/Madhura_Vaishnav/'
+})
 
 declare module '@tanstack/react-router' {
   interface Register {
